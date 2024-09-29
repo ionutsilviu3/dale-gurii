af30a1abf06d3102d795d5f6479a8c193bde884b
ionutsilviu3 <ionut.boanc@student.unitbv.ro>
2024-09-29 20:51:28 +0300

    feat: added legal disclaimer so i don't go to jail

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -10,8 +10,11 @@ const Footer = () => {
             <Link href="https://www.facebook.com/ceaunulmic"> <Image src={images.facebookIcon} className={styles.image} alt="Facebook"/> </Link>
             <Link href="https://www.instagram.com/ceaunulmic"> <Image src={images.instagramIcon} className={styles.image} alt="Instagram"/> </Link>
         </div>
+        {/* <h1 className="opensans-footer"> */}
+            {/* &copy;{(new Date().getFullYear())} Ceanul Mic. Toate drepturile rezervate.</h1> */}
         <h1 className="opensans-footer">
-            &copy;{(new Date().getFullYear())} Ceanul Mic. Toate drepturile rezervate.</h1>
+        Acest site web este un mock-up creat doar în scop demonstrativ. Logo-urile, imaginile, meniurile și alte materiale utilizate pe acest site sunt proprietatea deținătorilor respectivi.</h1>
+        
         </div>
     );
 };
